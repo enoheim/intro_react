3ca65e6d444e3011bf609dabb2a5a1aa59b86cc0
enoheim <enoheim@gmail.com>
2021-02-12 18:32:29 +0900

    initial commit

---
+++ src/Blog.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import Article from './Article';
+import * as FooBar from './components/FooBar';
+import Hoge from './components/Hoge';
+
+const Blog = () => {
+    // componentDidMount() {
+    //     // ボタンがクリックされたらいいね数を加算する
+    //     document.getElementById('counter').addEventListener('click', this.countUp)
+    // }
+
+    // componentDidUpdate() {
+    //     if (this.state.count >= 10) {
+    //         this.setState({count: 0})
+    //     }
+    // }
+
+    // componentWillUnmount() {
+    //     document.getElementById('counter').removeEventListener('click', this.countUp)
+    // }
+
+    // countUp = () => {
+    //     this.setState((state) => {
+    //         return {count: this.state.count + 1 }
+    //     })
+    // }
+
+    return (
+        <>
+            <Article
+                title={"Reactの使い方"}
+                // count={this.state.count}
+            />
+            <FooBar.Foo />
+            <FooBar.Bar />
+            <Hoge />
+        </>
+    )
+};
+
+export default Blog;
---
+++ src/components/FooBar.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Foo() {
+    return(<h2>A</h2>)
+};
+
+export const Bar = () => {
+    return(<h2>B</h2>)
+};
